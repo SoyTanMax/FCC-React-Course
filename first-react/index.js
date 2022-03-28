8f2fdeed2b28bbad04828129f38f0ba325d1753b
Max Lopez <soytanmax10@gmail.com>
2022-03-28 15:24:57 +0200

    first commit

---
+++ first-react/index.js
@@ -0,0 +1,16 @@
+const page = (
+    <div>
+        <h1>Website in React</h1>
+        <h3>Reasons to use react</h3>
+        <ol>
+            <li>Composable</li>
+            <li>Declarative</li>
+            <li>Hireable</li>
+            <li>Actively mantained</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(
+    page, document.getElementById("root")
+)
